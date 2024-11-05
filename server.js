cc0a9e7495f7a612a5bda36db2c36b5ad6b144c6
vinesk <vinesk@users.noreply.github.com>
2024-11-05 16:47:58 +0100

    Add search functionalities for products

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const shopProductsRouter = require("./routes/shop/products-routes");
 const shopCartRouter = require("./routes/shop/cart-routes");
 const shopAddressRouter = require("./routes/shop/address-routes");
 const shopOrderRouter = require("./routes/shop/order-routes");
+const shopSearchRouter = require("./routes/shop/search-routes");
 
 mongoose
   .connect("mongodb+srv://vinesk:XA3Mjb3X!@cluster0.f4oes.mongodb.net/")
@@ -45,6 +46,7 @@ app.use("/api/shop/products", shopProductsRouter);
 app.use("/api/shop/cart", shopCartRouter);
 app.use("/api/shop/address", shopAddressRouter);
 app.use("/api/shop/order", shopOrderRouter);
+app.use("/api/shop/search", shopSearchRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is now running on port ${PORT}`);
