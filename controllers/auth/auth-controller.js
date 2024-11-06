474b3f968ec8e7aefffa3ae06fb9e4992bcd6e49
vinesk <vinesk@users.noreply.github.com>
2024-11-06 16:52:06 +0100

    Change cookie config

--- controllers/auth/auth-controller.js
+++ controllers/auth/auth-controller.js
@@ -75,7 +75,7 @@ const loginUser = async (req, res) => {
     res
       .cookie("token", token, {
         httpOnly: true,
-        secure: false,
+        secure: true,
       })
       .json({
         success: true,
